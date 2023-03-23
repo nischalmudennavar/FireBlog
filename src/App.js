dc42da18721af513baa21780621b82188b8ac3af
Nischal Jagadish Mudennavar <nischalmudennavar@gmail.com>
2023-03-23 12:02:58 +0530

    router fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import "./app.css";
-
 import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/loginPage/Login";
 import Home from "./pages/Home/Home";
 import CreatePost from "./pages/CreatePost/CreatePost";
 import Navbar from "./Components/Navbar";
+import "./app.css";
 
 function App() {
   const [isAuth, setIsAuth] = useState(localStorage.getItem("isAuth"));
